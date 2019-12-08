fb4d5f9a09551f800554ebfaba354d98a9adb8b3
Valerii Bulakh <valerabulakh@gmail.com>
2019-12-08 05:52:02 -0600

    fix burger

--- src/index.js
+++ src/index.js
@@ -7,25 +7,25 @@ import 'es6-promise';
 import 'fetch-polyfill';
 
 import openPopUp from './modules/openPopUp';
-// import formsToSend from './modules/sendForm';
+import formsToSend from './modules/sendForm';
 import mainSlider from './modules/mainSlider';
 import servicesSlider from './modules/servicesSlide';
 import gallerySlider from './modules/gallerySlider';
-// import calc from './modules/calc';
+import calc from './modules/calc';
 import getSailAnchor from './modules/getSailAnchor';
 import scrollTop from './modules/scrollTop';
-import otherForm from './modules/otherForm';
+// import otherForm from './modules/otherForm';
 import burgerMenu from './modules/burgerMenu';
 
-// formsToSend();
+formsToSend();
 getSailAnchor();
 openPopUp();
 scrollTop();
 burgerMenu();
-otherForm();
+// otherForm();
 
 mainSlider();
 servicesSlider();
 gallerySlider();
 
-// calc();
+calc();
