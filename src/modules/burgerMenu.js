9b7c871c3480b2c924ed7ea03984aa75582cba65
Valerii Bulakh <valerabulakh@gmail.com>
2019-12-08 05:00:31 -0600

    fix probobly all

--- src/modules/burgerMenu.js
+++ src/modules/burgerMenu.js
@@ -1,27 +1,38 @@
 const burgerMenu = () =>{
-    const menuBtn = document.querySelector('.menu-button'),
+    const burgerBtn = document.querySelector('.menu-button'),
         menu = document.querySelector('.popup-menu'),
-        closeBtn = document.querySelector('.close-menu-btn');
+        closeBtn = document.querySelector('.close-menu-btn'),
+        topMenu = document.querySelector('.top-menu'),
+        menuLinks = document.querySelectorAll('.popup-menu > ul > li > a'),
+        breakpoint = topMenu.getBoundingClientRect().y;
 
-        let fat = document.documentElement.clientWidth;
-        if(fat >= 768){
-            menuBtn.style.cssText = `
-            position: fixed;
-            display: inline;
-            top: 0px;
-            `;
-        }
+    burgerBtn.addEventListener('click', () => {
+        menu.style.display = "block";
+    });
 
-        const activeBurger = () => {
-            if(!menu.style.display || menu.style.display === 'none'){
-                menu.style.display = 'flex';
-            }else{
-                menu.style.display = 'none';
+    closeBtn.addEventListener('click', () => {
+        menu.style.display = "none";
+    });
+
+    menuLinks.forEach((elem) => {
+        elem.addEventListener('click', () => {
+            menu.style.display = "none";
+        });
+    });
+
+    window.addEventListener('scroll', () => {
+        if (document.documentElement.clientWidth <= 768) {
+            if (topMenu.getBoundingClientRect().y < 0) {
+                burgerBtn.closest('.top-menu').classList.add('fixed-menu__button');
             }
-        };
+            if (window.scrollY < breakpoint) {
+                burgerBtn.closest('.top-menu').classList.remove('fixed-menu__button');
+            }
+        } else if (document.documentElement.clientWidth > 769) {
+            burgerBtn.closest('.top-menu').classList.remove('fixed-menu__button');
+        }
+    });
 
-        menuBtn.addEventListener('click', activeBurger);
-        closeBtn.addEventListener('click', activeBurger);
-       
 };
+
 export default burgerMenu;
